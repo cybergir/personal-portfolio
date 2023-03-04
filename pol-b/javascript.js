d45213e2675276ec1921c771d2cee7d6bc32ceae
Ruby <dorothymwende505@gmail.com>
2023-03-04 12:33:39 +0300

    This is my first commit fot this project

---
+++ pol-b/javascript.js
@@ -0,0 +1,7 @@
+// ===================== typing animation =========================
+var typed = new Typed(".typing", {
+    strings:["", "Full Stack Developer", , "Software Engineer", "Graphics Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
